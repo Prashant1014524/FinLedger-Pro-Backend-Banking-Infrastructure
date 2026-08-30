7bcf02b6f07fef6ef3b95d84af9e12123de760fc
Prashant Uniyal <prashantuniyal22@gmail.com>
2026-08-31 02:31:47 +0530

    black list token done

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ const authRouter=require("./routes/auth.routes")
 const accountRouter=require("./routes/account.routes")
 const transactionRoutes=require("./routes/transaction.routes")
 // use routes
+app.get("/",(req,res)=>{
+    res.send("Ledger Service is up and running")
+})
 app.use("/api/auth",authRouter) //jin bhi api ka end point /api/auth hit hoga sb authroute.js pr route hongi
 app.use("/api/accounts",accountRouter)
 app.use("/api/transactions",transactionRoutes)
